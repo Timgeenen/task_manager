b6b1b3615c9c5af8cc7014099f98f2e604cfc09d
TIM GEENEN <timmiej_95@hotmail.com>
2024-08-29 11:17:04 +0700

    chore: moved client files

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,8 +3,8 @@
 function Navbar() {
 
   return (
-    <div className="w-full">
-      <img src="" />
+    <div className="w-full h-6 bg-red-600 fixed top-0">
+      HERRO
       {/* Logo */}
       {/* Links */}
       {/* Profile */}
