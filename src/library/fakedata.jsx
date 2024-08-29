b9c621cc82fbea9948df1c10683664cef91208ca
TIM GEENEN <timmiej_95@hotmail.com>
2024-08-29 15:02:43 +0700

    feat: added fake data file

---
+++ src/library/fakedata.jsx
@@ -0,0 +1,12 @@
+const TEAMS = [
+  {
+    title: "Management",
+    id: "124809"
+  }, {
+    title: "Administration",
+    id: "849012"
+  }, {
+    title: "Development",
+    id: "724352"
+  }
+]
