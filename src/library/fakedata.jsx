6855ff4854e1f308f0aeb2cb07e6992b8183bbbb
TIM GEENEN <timmiej_95@hotmail.com>
2024-08-29 15:28:58 +0700

    refactor: updated test data

--- src/library/fakedata.jsx
+++ src/library/fakedata.jsx
@@ -1,12 +1,15 @@
-const TEAMS = [
+export const TEAMS = [
   {
     title: "Management",
+    members: ["Tim Geenen", "Aimi Takanaga"],
     id: "124809"
   }, {
     title: "Administration",
+    members: ["Tim Geenen", "Rick Kamphuis", "Kai Jakobi"],
     id: "849012"
   }, {
     title: "Development",
+    members: ["Tim Geenen", "Elon Musk", "Jef Bezos"],
     id: "724352"
   }
-]
+];
