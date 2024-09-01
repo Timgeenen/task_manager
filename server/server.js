898ea4136a7364cd8054801211a136460d2d9f89
TIM GEENEN <timmiej_95@hotmail.com>
2024-09-01 18:49:37 +0700

    feat: created data models for database

--- server/server.js
+++ server/server.js
@@ -9,8 +9,94 @@ const cors = require("cors");
 const corsOptions = { origin: "http://localhost:3000" }
 app.use(cors(corsOptions));
 
-app.get("/api", (req, res) => {
-  res.json({fruits: ["apple", "banana"]});
+
+
+const userSchema = new mongoose.Schema({
+  name: String,
+  role: String,
+  emal: String,
+  createdOn: Date,
+  lastUpdated: Date,
+  isAdmin: Boolean,
+  isActive: Boolean,
+  teams: [{
+    name: String,
+    id: String
+  }]
+});
+
+const teamSchema = new mongoose.Schema({
+  name: String,
+  manager: {
+    name: String,
+    id: String
+  },
+  members: [{
+    name: String,
+    id: String
+  }],
+  tasks: [{
+    title: String,
+    assignedTo: [{
+      name: String,
+      id: String
+    }],
+    id: String
+  }],
+  createdOn: Date,
+  messages: [{
+    author: {
+      name: String,
+      id: String
+    },
+    message: String,
+    createdOn: Date
+  }],
+  trash: [Map]
+});
+
+const taskSchema = new mongoose.Schema({
+  title: String,
+  description: String,
+  subTasks: [{
+    description: String,
+    completed: {
+      type: Boolean,
+      default: false
+    }
+  }],
+  createdOn: Date,
+  deadline: Date,
+  priority: String,
+  status: String,
+  assignedTo: [{
+    name: String,
+    id: String
+  }],
+  comments: [{
+    author: {
+      name: String,
+      id: String
+    },
+    message: String,
+    createdOn: Date
+  }],
+  updates: [{
+    author: {
+      name: String,
+      id: String,
+    },
+    message: String,
+    updatedOn: Date
+  }]
+});
+
+let User = mongoose.model('user', userSchema);
+let Team = mongoose.model('team', teamSchema);
+let Task = mongoose.model('task', taskSchema);
+
+app.get("/api:index", async (req, res) => {
+
 });
 
 app.listen(8080, () => {
