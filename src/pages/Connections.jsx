42e2b241aae6b294646b470f454051b23894c531
TIM GEENEN <timmiej_95@hotmail.com>
2024-09-03 18:40:51 +0700

    feat: added basic layout for connections page

--- src/pages/Connections.jsx
+++ src/pages/Connections.jsx
@@ -1,9 +1,20 @@
+import { useSelector } from "react-redux"
 
 
 function Connections() {
+  const { user } = useSelector(state => state.auth);
+
   return (
-    <div>
-      Connections
+    <div className="w-full p-2">
+      {
+        user.connections.map(item => (
+          <div className="w-full flex justify-between border-2 border-slate-400 p-2">
+            <span>{item.name}</span>
+            <span>{item.email}</span>
+            <span>{item.role}</span>
+          </div>
+        ))
+      }
     </div>
   )
 }
