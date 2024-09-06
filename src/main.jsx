d600699bcdc3cdcb8d45d7b8c19024b6bad7a34c
TIM GEENEN <timmiej_95@hotmail.com>
2024-09-06 16:55:03 +0700

    feat: wrapped project inside queryclientprovider

--- src/main.jsx
+++ src/main.jsx
@@ -5,15 +5,19 @@ import './index.css'
 import { Provider } from 'react-redux'
 import { store } from './redux/store'
 import { BrowserRouter } from 'react-router-dom'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
+const queryClient = new QueryClient();
 window.React = React;
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>
+    <QueryClientProvider client={queryClient}>
+      <Provider store={store}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </Provider>
+    </QueryClientProvider>
   </StrictMode>,
 )
