e6a14554673d13d0f6aee7b817951d282ba55bc9
TIM GEENEN <timmiej_95@hotmail.com>
2024-09-07 20:33:05 +0700

    refactor: extracted all api calls into global api file

--- src/api/Event.js
+++ src/api/Event.js
@@ -1,9 +1,47 @@
 import axios from "axios";
 import { BACKEND } from "../library/constants";
+import { store } from "../redux/store";
 
 axios.defaults.baseURL = BACKEND;
 
+export const authenticateUser = async (userData) => {
+  const res = await axios.post("/login", userData);
+  return res.data;
+}
+
+export const createUser = async (userData) => {
+  const res = await axios.post("/register", userData);
+  return res.data;
+}
+
 export const getTaskById = async (taskId) => {
   const res = await axios.get("/task" + taskId);
+  return res.data;
+};
+
+export const createNewTeam = async (teamData) => {
+  const res = await axios.post("/create-team", teamData);
+  return res.data;
+};
+
+export const addConnection = async (id) => {
+  const userId = store.getState().auth.user._id
+
+  const userData = {
+    user: userId,
+    id,
+  };
+
+  const res = await axios.put("/add-connection", userData);
+  return res.data;
+};
+
+export const getAllUsers = async () => {
+  const res = await axios.get("/connections");
+  return res.data;
+};
+
+export const createNewTask = async (taskData) => {
+  const res = await axios.post("/create-task", taskData);
   return res.data;
 }
