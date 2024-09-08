f9c6ad33eb49c0da39a2562c622a18cd98a83000
TIM GEENEN <timmiej_95@hotmail.com>
2024-09-08 13:51:16 +0700

    feat: added api route for fetching task arrays connected to each team in array of team ids

--- src/api/Event.js
+++ src/api/Event.js
@@ -7,25 +7,35 @@ axios.defaults.baseURL = BACKEND;
 export const authenticateUser = async (userData) => {
   const res = await axios.post("/login", userData);
   return res.data;
-}
+};
 
 export const createUser = async (userData) => {
   const res = await axios.post("/register", userData);
   return res.data;
-}
+};
 
 export const getTaskById = async (taskId) => {
   const res = await axios.get("/task" + taskId);
   return res.data;
 };
 
+export const getTeamTaskArr = async (teamIds) => {
+  const res = await axios.post("/get-team-tasksArr", teamIds);
+  return res.data;
+}
+
 export const createNewTeam = async (teamData) => {
   const res = await axios.post("/create-team", teamData);
   return res.data;
 };
 
+// export const getTeamsByIds = async (teamIds) => {
+//   const res = await axios.post("/get-teams", teamIds);
+//   return res.data;
+// };
+
 export const addConnection = async (id) => {
-  const userId = store.getState().auth.user._id
+  const userId = store.getState().auth.user._id;
 
   const userData = {
     user: userId,
@@ -44,4 +54,4 @@ export const getAllUsers = async () => {
 export const createNewTask = async (taskData) => {
   const res = await axios.post("/create-task", taskData);
   return res.data;
-}
+};
