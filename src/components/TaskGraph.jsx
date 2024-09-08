be0168d457c84c4dc46b6d29a7ea71518a22f4b2
TIM GEENEN <timmiej_95@hotmail.com>
2024-09-08 13:54:38 +0700

    refactor: updated component to receive data from api and display data based on team selected

--- src/components/TaskGraph.jsx
+++ src/components/TaskGraph.jsx
@@ -1,42 +1,26 @@
-import { BarChart, Legend, Tooltip, XAxis, YAxis, Bar, CartesianGrid, Cell } from "recharts";
-import { tasks } from "../library/fakedata";
-//TODO: change fake to real data
-//TODO: switch between teams to show data per team
-//TODO: change general color for bars
+import { BarChart, Tooltip, XAxis, YAxis, Bar, CartesianGrid, Cell } from "recharts";
+import { countTasksByPriority } from "../library/helperfunctions";
+import { useEffect, useState } from "react";
 //TODO: make chart responsive
 
-function TaskGraph() {
-  let data = [
-    {
-      priority: "high",
-      tasks: 0,
-      color: "red"
-    }, {
-      priority: "medium",
-      tasks: 0,
-      color: "orange"
-    }, {
-      priority: "low",
-      tasks: 0,
-      color: "green"
-    }
-  ]
+function TaskGraph({data}) {
 
-  tasks.filter((task) => {
-    if (task.priority === "high") { return data[0].tasks += 1 }
-    if (task.priority === "medium") { return data[1].tasks += 1 }
-    if (task.priority === "low") { return data[2].tasks += 1 }
-  })
+  const [graphData, setGraphData] = useState([]);
+  
+  useEffect(() => {
+    const newData = countTasksByPriority(data)
+    setGraphData(newData);
+  }, []);
 
   return (
-    <BarChart width={700} height={300} data={data}>
+    <BarChart width={700} height={300} data={graphData}>
       <CartesianGrid strokeDasharray="3 3"/>
       <XAxis dataKey="priority" color="red"/>
       <YAxis />
       <Tooltip />
       <Bar 
       dataKey="tasks">
-        {data.map((item, i) => (
+        {graphData.map((item, i) => (
           <Cell key={`cell-${i}`} fill={item.color} />
         ))}
       </Bar>
