8abfabb7548d993ce74d08c575df21be1faeec1e
TIM GEENEN <timmiej_95@hotmail.com>
2024-09-08 19:25:12 +0700

    feat: added chatroom to task info component

--- src/pages/TaskInfo.jsx
+++ src/pages/TaskInfo.jsx
@@ -1,9 +1,10 @@
 import { useParams } from "react-router-dom";
-import { useForm } from "react-hook-form";
 import { getTaskById } from "../api/Event";
 import { useQuery } from "@tanstack/react-query";
 import MembersTag from "../components/MembersTag";
 import { getTimePassed } from "../library/helperfunctions";
+import Commentbox from "../components/Commentbox";
+import Comment from "../components/Comment";
 
 function TaskInfo() {
   const { taskId } = useParams();
@@ -13,19 +14,16 @@ function TaskInfo() {
     staleTime: Infinity
   });
 
-  const { register, control } = useForm();
-
   const updateTask = (data) => {
     console.log(data);
   }
 
   if (isPending) { return ( <div>Loading...</div> ) };
   if (error) { return ( <div>{error.message}</div> ) };
-  
-  console.log(data)
+  data && console.log(data)
   return (
-    <div className="w-10/12 flex flex-col justify-between m-auto">
-      <div className="flex gap-8 items-center">
+    <div className="w-10/12 flex flex-col justify-between m-auto border-2">
+      <div className="flex justify-between items-center w-full border-2">
         <span>{data?.title}</span>
         <span>{data?.assignedTeam.name}</span>
         <span className="mr-4">{data?.assignedTo.map((member, i) => (
@@ -38,14 +36,23 @@ function TaskInfo() {
         <span>{data?.priority}</span>
         <span>{data?.status}</span>
       </div>
-      <div>
+      <div className="flex flex-col gap-2 h-40 overflow-y-scroll w-full">
         {data?.comments.map((comment, i) => (
-          <>
-            <span>{comment.author}</span>
-            <span>{comment.message}</span>
-            <span>{getTimePassed(comment.createdAt)}</span></>
+          <Comment
+          authorName={comment.author.name}
+          authorId={comment.author.id}
+          message={comment.message}
+          commentId={comment._id}
+          date={comment.createdAt}
+          />
+          // <div>
+          //   <span>{comment.author.name}</span>
+          //   <span>{comment.message}</span>
+          //   <span>{getTimePassed(comment.createdAt)} ago</span>
+          // </div>
         ))}
       </div>
+      <Commentbox taskId={taskId} />
       <button
       className="border-2 w-40 p-3"
       onClick={updateTask}>UPDATE TASK</button>
