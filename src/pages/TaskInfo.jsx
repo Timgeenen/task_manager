127ba9f09b0f456be9eed6677eefc3a9ebc8222f
TIM GEENEN <timmiej_95@hotmail.com>
2024-09-09 19:18:14 +0700

    feat: added functioning chatroom component

--- src/pages/TaskInfo.jsx
+++ src/pages/TaskInfo.jsx
@@ -2,13 +2,18 @@ import { useParams } from "react-router-dom";
 import { getTaskById } from "../api/Event";
 import { useQuery } from "@tanstack/react-query";
 import MembersTag from "../components/MembersTag";
-import { getTimePassed } from "../library/helperfunctions";
-import Commentbox from "../components/Commentbox";
-import Comment from "../components/Comment";
+import Chatroom from "../components/Chatroom";
 
 function TaskInfo() {
   const { taskId } = useParams();
-  const { isPending, error, data } = useQuery({
+
+  const {
+    isPending,
+    isError,
+    isSuccess,
+    error,
+    data
+  } = useQuery({
     queryKey: [`task-${taskId}`],
     queryFn: () => getTaskById(taskId),
     staleTime: Infinity
@@ -19,43 +24,35 @@ function TaskInfo() {
   }
 
   if (isPending) { return ( <div>Loading...</div> ) };
-  if (error) { return ( <div>{error.message}</div> ) };
-  data && console.log(data)
+  if (isError) { return ( <div>{error.message}</div> ) };
+
   return (
     <div className="w-10/12 flex flex-col justify-between m-auto border-2">
       <div className="flex justify-between items-center w-full border-2">
         <span>{data?.title}</span>
         <span>{data?.assignedTeam.name}</span>
-        <span className="mr-4">{data?.assignedTo.map((member, i) => (
+        <span className="mr-4">
+          {data?.assignedTo.map((member, i) => (
           <MembersTag member={member.name} index={i} />
-        ))}</span>
+        ))}
+        </span>
       </div>
       <div className="flex gap-8 items-center">
         <textarea value={data?.description}></textarea>
-        {/* TODO: add checkbox for each subtask */}
         <span>{data?.priority}</span>
         <span>{data?.status}</span>
       </div>
-      <div className="flex flex-col gap-2 h-40 overflow-y-scroll w-full">
-        {data?.comments.map((comment, i) => (
-          <Comment
-          authorName={comment.author.name}
-          authorId={comment.author.id}
-          message={comment.message}
-          commentId={comment._id}
-          date={comment.createdAt}
-          />
-          // <div>
-          //   <span>{comment.author.name}</span>
-          //   <span>{comment.message}</span>
-          //   <span>{getTimePassed(comment.createdAt)} ago</span>
-          // </div>
-        ))}
+      <div>
+        <button
+        className="border-2 w-40 p-3"
+        onClick={updateTask}
+        >UPDATE TASK
+        </button>
       </div>
-      <Commentbox taskId={taskId} />
-      <button
-      className="border-2 w-40 p-3"
-      onClick={updateTask}>UPDATE TASK</button>
+      <Chatroom
+      taskId={taskId}
+      messagesArr={data.comments}
+      />
     </div>
   )
 }
