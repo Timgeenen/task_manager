372ed3a62e53c4dc687a0d753eb54ad59d0bcbec
TIM GEENEN <timmiej_95@hotmail.com>
2024-09-12 13:52:19 +0700

    refactor: updated component to use react query for fetching data

--- src/pages/Teams.jsx
+++ src/pages/Teams.jsx
@@ -1,15 +1,23 @@
-import { useSelector } from "react-redux";
 import AddButton from "../components/AddButton";
 import MembersTag from "../components/MembersTag";
 import { useNavigate } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
+import { getAllTeams } from "../api/Event";
 
 function Teams() {
-  const { user } = useSelector(state => state.auth);
   const navigate = useNavigate();
+
+  const { isLoading, isError, isSuccess, data, error } = useQuery({
+    queryKey: ["teams"],
+    queryFn: getAllTeams
+  })
+
+  if (isError) { console.error(error.message) };
 
   return (
     <div className="w-full flex flex-col gap-10">
-      {user.teams.map((team, i) => (
+      {isLoading && <div>Loading...</div>}
+      {isSuccess && data?.map((team, i) => (
           <div key={team.id} className="">
             <button className="w-1/4">{team.name}</button>
             <span>
