395b656e8eb75829635c9ba86fdad3971b252fd1
TIM GEENEN <timmiej_95@hotmail.com>
2024-09-12 16:56:07 +0700

    feat: configured react hook form to handle changes to task

--- src/components/TaskEdit.jsx
+++ src/components/TaskEdit.jsx
@@ -1,4 +1,9 @@
+import { useState } from "react";
+import AddButton from "./AddButton";
 import Checkbox from "./Checkbox";
+import { useForm, useFieldArray } from "react-hook-form";
+import SubmitButton from "./SubmitButton";
+import { IoClose } from "react-icons/io5";
 
 function TaskEdit({
   teamId,
@@ -8,30 +13,90 @@ function TaskEdit({
   priority,
   status
 }) {
+
+  const {
+    register,
+    control,
+    handleSubmit,
+    setValue
+  } = useForm({
+    defaultValues: {
+      subtasks: [{ name: "" }]
+    }
+  });
+
+  const { fields, append, remove } = useFieldArray({
+    control,
+    name: "subtasks"
+  })
+
+  const [newSubtask, setNewSubtask] = useState("");
+  const [subtasksArr, setSubtasksArr] = useState(subtasks);
+  const [error, setError] = useState(null);
+
+  const addSubtask = (e) => {
+    e.preventDefault();
+    if (newSubtask === "") { return setError("please add description")}
+    const newSubtaskObj = { name: newSubtask, completed: false };
+    append(newSubtaskObj);
+    setSubtasksArr([...subtasksArr, newSubtaskObj]);
+    document.getElementById("taskEdit__newSubtask").value = "";
+  };
+
+  const handleChange = (e) => {
+    error && setError(null);
+    setNewSubtask(e.target.value);
+  };
+
+  const submitHandler = (data) => {
+    console.log(data);
+  }
+
   return (
     <div>
-      <div className="flex gap-8 items-center">
+      <form
+      className="flex gap-8 items-center"
+      onSubmit={handleSubmit(submitHandler)}
+      >
         <textarea 
-        value={description}
+        defaultValue={description}
+        {...register("description")}
         className="w-full m-6 p-2 border-2 rounded-lg h-96 text-lg"
         ></textarea>
-        <div className="w-40 border-2 self-start m-6 ml-0 p-2 rounded-lg">
-          {subtasks.map((task, i) => (
-            <Checkbox
-            value={task._id}
-            text={task.name}
-            checked={task.completed}
-            />
-          ))}
+        <div className="w-40 border-2 self-start m-6 ml-0 p-2 rounded-lg flex flex-col">
+          {fields.map((field, i) => {
+            const task = subtasksArr[i];
+            setValue(`subtasks.${i}.name`, task.name)
+            return (
+              <span
+              key={field.id}
+              className="relative border-2 p-1 rounded-lg">
+                <Checkbox
+                text={task.name}
+                checked={task.completed}
+                register={register(`subtasks.${i}.completed`)}
+                />
+                {!task._id && <IoClose
+                onClick={() => { remove(i)} }
+                size={24}
+                className="absolute -right-7 top-1.5 hover:cursor-pointer"
+                />}
+              </span>
+            )
+          })}
+          <input
+          className="border-2"
+          id="taskEdit__newSubtask"
+          onChange={handleChange}
+          />
+          {error && <span className="text-xs text-red-600">{error}</span>}
+          <AddButton
+          text="Add Subtask"
+          handleClick={addSubtask}
+          />
         </div>
-      </div>
-      <div>
-        <button
-        className="border-2 w-40 p-2 rounded-lg font-semibold m-6 mt-0"
-        onClick={() => console.log("updated")}
-        >UPDATE TASK
-        </button>
-      </div>
+        <SubmitButton />
+      </form>
     </div>
   )
 }
