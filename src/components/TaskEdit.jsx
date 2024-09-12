0c92a40d7915aa3f0c035ba9a9b0d22147d7c8ad
TIM GEENEN <timmiej_95@hotmail.com>
2024-09-12 18:33:11 +0700

    refactor: updated defaultvalues to be able to check if form is dirty and display an error accordingly

--- src/components/TaskEdit.jsx
+++ src/components/TaskEdit.jsx
@@ -11,21 +11,30 @@ function TaskEdit({
   description,
   subtasks,
   priority,
-  status
+  status,
 }) {
 
   const {
     register,
     control,
     handleSubmit,
-    setValue
+    setValue,
+    formState: { isDirty }
   } = useForm({
     defaultValues: {
-      subtasks: [{ name: "" }]
+      subtasks,
+      priority,
+      status,
+      description,
+      completed: false
     }
   });
 
-  const { fields, append, remove } = useFieldArray({
+  const {
+    fields,
+    append,
+    remove,
+  } = useFieldArray({
     control,
     name: "subtasks"
   })
@@ -33,13 +42,15 @@ function TaskEdit({
   const [newSubtask, setNewSubtask] = useState("");
   const [subtasksArr, setSubtasksArr] = useState(subtasks);
   const [error, setError] = useState(null);
+  const [submitError, setSubmitError] = useState(null);
 
   const addSubtask = (e) => {
     e.preventDefault();
-    if (newSubtask === "") { return setError("please add description")}
+    if (newSubtask === "") { return setError("please add description")};
     const newSubtaskObj = { name: newSubtask, completed: false };
     append(newSubtaskObj);
     setSubtasksArr([...subtasksArr, newSubtaskObj]);
+    setNewSubtask("");
     document.getElementById("taskEdit__newSubtask").value = "";
   };
 
@@ -49,8 +60,14 @@ function TaskEdit({
   };
 
   const submitHandler = (data) => {
-    console.log(data);
+    if (isDirty) {
+
+    } else {
+      setSubmitError("Please adjust the form before submitting");
+    }
   }
+
+  if (isDirty) { submitError && setSubmitError(null)};
 
   return (
     <div>
@@ -63,39 +80,51 @@ function TaskEdit({
         {...register("description")}
         className="w-full m-6 p-2 border-2 rounded-lg h-96 text-lg"
         ></textarea>
-        <div className="w-40 border-2 self-start m-6 ml-0 p-2 rounded-lg flex flex-col">
-          {fields.map((field, i) => {
-            const task = subtasksArr[i];
-            setValue(`subtasks.${i}.name`, task.name)
-            return (
-              <span
-              key={field.id}
-              className="relative border-2 p-1 rounded-lg">
-                <Checkbox
-                text={task.name}
-                checked={task.completed}
-                register={register(`subtasks.${i}.completed`)}
-                />
-                {!task._id && <IoClose
-                onClick={() => { remove(i)} }
-                size={24}
-                className="absolute -right-7 top-1.5 hover:cursor-pointer"
-                />}
-              </span>
-            )
-          })}
-          <input
-          className="border-2"
-          id="taskEdit__newSubtask"
-          onChange={handleChange}
+        <div>
+          <div className="w-40 self-start m-6 ml-0 rounded-lg flex flex-col">
+            {fields.map((field, i) => {
+              const task = subtasksArr[i];
+              task._id && setValue(`subtasks.${i}._id`, task._id);
+              setValue(`subtasks.${i}.name`, task.name)
+              return (
+                <span
+                key={field.id}
+                className="relative border-2 p-1 rounded-lg">
+                  <Checkbox
+                  text={task.name}
+                  checked={task.completed}
+                  register={register(`subtasks.${i}.completed`)}
+                  />
+                  {!task._id && <IoClose
+                  onClick={() => { remove(i)} }
+                  size={24}
+                  className="absolute -right-7 top-1.5 hover:cursor-pointer"
+                  />}
+                </span>
+              )
+            })}
+            
+            <input
+            className="border-2"
+            id="taskEdit__newSubtask"
+            placeholder="Add description"
+            onChange={handleChange}
+            />
+            {error && <span className="text-xs text-red-600">{error}</span>}
+            <AddButton
+            text="Add Subtask"
+            handleClick={addSubtask}
+            />
+          </div>
+          <div className="flex flex-col items-center">
+          <Checkbox
+          register={register("completed")}
+          text="Complete Task"
           />
-          {error && <span className="text-xs text-red-600">{error}</span>}
-          <AddButton
-          text="Add Subtask"
-          handleClick={addSubtask}
-          />
+          <SubmitButton />
+          {submitError && <div className="text-xs text-red-600 text-center">{submitError}</div>}
+          </div>
         </div>
-        <SubmitButton />
       </form>
     </div>
   )
