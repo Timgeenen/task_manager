05a5dc40f2eb12a193fe39622697647d2e7659c3
TIM GEENEN <timmiej_95@hotmail.com>
2024-09-14 19:20:25 +0700

    feat: added multiple inputs to form to set params for filtering tasks

--- src/pages/Tasks.jsx
+++ src/pages/Tasks.jsx
@@ -1,13 +1,15 @@
-import { useNavigate } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
 import { getAllTasks } from "../api/Event";
 import { sortTasksByDeadline } from "../library/helperfunctions";
 import TaskList from "../components/TaskList";
 import { useForm } from "react-hook-form";
 import DateSelect from "../components/DateSelect.jsx";
+import { useSelector } from "react-redux";
+import Optionbox from "../components/Optionbox.jsx";
+import { LuRefreshCcw } from "react-icons/lu";
 
 function Tasks () {
-  const navigate = useNavigate();
+  const teams = useSelector(state => state.auth.user.teams);
 
   const { isLoading, isError, isSuccess, error, data } = useQuery({
     queryKey: ["all-tasks"],
@@ -18,22 +20,19 @@ function Tasks () {
   const currentStatus = watch("status");
   const fromDate = watch("from");
   const toDate = watch("to");
+  const currentTeam = watch("team");
+  const currentPriority = watch("priority");
 
   const headers = ["Title", "Deadline", "Priority", "Status", "Team", "Members"];
   const status = ["in progress", "pending", "completed"];
+  const priority = ["high", "medium", "low"];
 
   if (isError) { alert(error.message) };
   if (isSuccess) { sortTasksByDeadline(data) };
 
   return (
     <div className="w-5/6 m-auto mt-4">
-      <div>
-        <select {...register("status")}>
-          <option value="all">All Tasks</option>
-          {status.map(stat => (
-            <option value={stat}>{stat}</option>
-          ))}
-        </select>
+      <div className="flex justify-between mt-2 mb-2">
         <DateSelect
         control={control}
         text="From"
@@ -46,6 +45,27 @@ function Tasks () {
         name="to"
         minDate={fromDate}
         />
+        <Optionbox
+        options={status}
+        register={register("status")}
+        defaultText="--Select Status"
+        defaultValue="all"
+        />
+        <Optionbox
+        options={priority}
+        register={register("priority")}
+        defaultText="--Select Priority"
+        defaultValue="all"
+        />
+        <Optionbox
+        options={teams}
+        register={register("team")}
+        defaultValue="all"
+        defaultText="--Select Team"
+        />
+        <button onClick={reset}>
+          <LuRefreshCcw size={24}/>
+        </button>
       </div>
       {isLoading && <div>Loading...</div>}
       <div className="sticky grid grid-flow-col auto-cols-fr items-center bg-blue-400">
@@ -61,7 +81,11 @@ function Tasks () {
       {isSuccess && 
         <TaskList
         data={data}
-        filter={currentStatus}
+        status={currentStatus}
+        from={fromDate}
+        to={toDate}
+        team={currentTeam}
+        priority={currentPriority}
         />
       }
     </div>
