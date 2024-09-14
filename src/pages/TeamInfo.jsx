c303df7dbaf04ea2edd90a0d7fb27a9af612d11a
TIM GEENEN <timmiej_95@hotmail.com>
2024-09-14 12:23:23 +0700

    chore: added text

--- src/pages/TeamInfo.jsx
+++ src/pages/TeamInfo.jsx
@@ -1,8 +1,11 @@
+import { useParams } from "react-router-dom"
 
 function TeamInfo() {
+  const { teamId } = useParams();
+  console.log(teamId);
   return (
     <div>
-      
+      Team Info
     </div>
   )
 }
