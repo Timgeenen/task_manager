d23110121c9f8169c7322fae1707a1e827fa3d14
TIM GEENEN <timmiej_95@hotmail.com>
2024-09-14 10:50:28 +0700

    feat: added link to relevant route to notification and update emit to database

--- src/components/NotificationLink.jsx
+++ src/components/NotificationLink.jsx
@@ -1,27 +1,39 @@
-import { memo, useState } from "react";
+import { memo } from "react";
 import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 function NotificationLink({
 message,
 type,
-isNew,
 teamName,
 teamId,
 taskName,
 taskId,
 userName,
-userId
+userId,
+notificationId,
+index,
+updateQuery
 }) {
-  const [unread, setUnread] = useState(isNew);
+  const { socket } = useSelector(state => state.auth);
   const navigate = useNavigate();
+
+  const id = teamId ? teamId : taskId ? taskId : userId;
+  const route = teamId ? `/team-info/${id}` : taskId ? `/task-info/${id}` : `/profile/${id}`
+  const title = teamName ? teamName : taskName ? taskName : userName;
+
   const handleClick = () => {
-    //TODO: create navigate route based on notification type
-    console.log("navigated");
+    socket.emit("readNotification", userId, notificationId, (response) => {
+      if (response.error) { return console.error(response.error.message) };
+      updateQuery(index);
+      navigate(route)
+    });
   };
 
-  const title = teamName ? teamName : taskName ? taskName : userName;
   return (
     <button
+    key={notificationId}
+    id={notificationId}
     onClick={handleClick}
     className="mb-1 border-2 p-1 rounded-lg">
       <div className="text-sm flex justify-between font-semibold pb-1">
