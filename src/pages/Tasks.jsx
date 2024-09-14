4f36ec0020c1c056a9331c6d314022c21c2fb55a
TIM GEENEN <timmiej_95@hotmail.com>
2024-09-14 17:47:25 +0700

    feat: added react hook form to handle input state for filtering tasks

--- src/pages/Tasks.jsx
+++ src/pages/Tasks.jsx
@@ -2,8 +2,9 @@ import { useNavigate } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
 import { getAllTasks } from "../api/Event";
 import { sortTasksByDeadline } from "../library/helperfunctions";
-import { useState } from "react";
 import TaskList from "../components/TaskList";
+import { useForm } from "react-hook-form";
+import DateSelect from "../components/DateSelect.jsx";
 
 function Tasks () {
   const navigate = useNavigate();
@@ -13,7 +14,10 @@ function Tasks () {
     queryFn: () => getAllTasks(),
   });
 
-  const [filter, setFilter] = useState("all");
+  const { register, control, reset, watch } = useForm({})
+  const currentStatus = watch("status");
+  const fromDate = watch("from");
+  const toDate = watch("to");
 
   const headers = ["Title", "Deadline", "Priority", "Status", "Team", "Members"];
   const status = ["in progress", "pending", "completed"];
@@ -24,12 +28,24 @@ function Tasks () {
   return (
     <div className="w-5/6 m-auto mt-4">
       <div>
-      <select onChange={(e) => setFilter(e.target.value)}>
-        <option value="all">All Tasks</option>
-        {status.map(stat => (
-          <option value={stat}>{stat}</option>
-        ))}
-      </select>
+        <select {...register("status")}>
+          <option value="all">All Tasks</option>
+          {status.map(stat => (
+            <option value={stat}>{stat}</option>
+          ))}
+        </select>
+        <DateSelect
+        control={control}
+        text="From"
+        name="from"
+        maxDate={toDate}
+        />
+        <DateSelect
+        control={control}
+        text="To"
+        name="to"
+        minDate={fromDate}
+        />
       </div>
       {isLoading && <div>Loading...</div>}
       <div className="sticky grid grid-flow-col auto-cols-fr items-center bg-blue-400">
@@ -45,7 +61,7 @@ function Tasks () {
       {isSuccess && 
         <TaskList
         data={data}
-        filter={filter}
+        filter={currentStatus}
         />
       }
     </div>
