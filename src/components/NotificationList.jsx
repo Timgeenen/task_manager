166ae74452d136344890c41e11e0ea60146831e6
TIM GEENEN <timmiej_95@hotmail.com>
2024-09-16 18:58:28 +0700

    feat: added mark all as read function

--- src/components/NotificationList.jsx
+++ src/components/NotificationList.jsx
@@ -1,26 +1,38 @@
-import { useQuery } from "@tanstack/react-query"
+import React from "react";
+import { useQuery, useQueryClient } from "@tanstack/react-query"
 import { getAllNotifications } from "../api/Event";
 import { getTimePassed } from "../library/helperfunctions";
 import useNavigateNotification from "../hooks/useNavigateNotification";
+import clsx from "clsx";
+import { useSelector } from "react-redux";
 
 function NotificationList() {
-  const { isLoading, isError, isSuccess, error, data } = useQuery({
+  const { isLoading, isError, isSuccess, error, data, refetch } = useQuery({
     queryKey: ["all-notifications"],
-    queryFn: getAllNotifications
+    queryFn: () => getAllNotifications(false),
   });
+  const { socket } = useSelector(state => state.auth);
+
+  const markAllAsRead = () => {
+    socket.emit("markAllAsRead", (response) => {
+      console.log(response)
+      if(response.err) { return console.err(response.err) };
+      refetch();
+    });
+  }
 
   if(isError) {console.log(error.message)};
-  data && console.log(data)
+
   return (
-    <div className="w-2/3 m-auto">
+    <div className="w-2/3 m-auto border-2 relative rounded-lg">
       {isLoading && <div>Loading...</div>}
-      <div className="grid grid-cols-6 p-2 border-2 font-semibold">
+      <div className="grid grid-cols-6 p-2 bg-blue-400 font-semibold rounded-t-md">
         <span className="col-span-1">Title</span>
         <span className="col-span-1">Type</span>
         <span className="col-span-1">Received</span>
         <span className="col-span-3 text-center">Message</span>
       </div>
-      <div className="overflow-y-scroll h-80">
+      <div className="overflow-y-scroll h-80 pl-1 pr-1">
       {isSuccess && data?.map((item) => (
         <CustomLink
         createdAt={item.createdAt}
@@ -31,15 +43,22 @@ function NotificationList() {
         user={item.user}
         type={item.nType}
         notificationId={item._id}
+        updateFn={refetch}
         />
       ) )}
       </div>
+      <button
+      className="text-xs text-blue-500 absolute -bottom-6"
+      onClick={markAllAsRead}
+      >
+        Mark all as read
+      </button>
     </div>
   )
 }
 
 const CustomLink = React.memo(({
-  createdAt, isRead, message, type, task, team, user, notificationId
+  createdAt, isRead, message, type, task, team, user, notificationId, updateFn
 }) => {
   const title = task ? task.name : team ? team.name : user.name;
 
@@ -47,12 +66,13 @@ const CustomLink = React.memo(({
     teamId: team?.id,
     taskId: task?.id,
     userId: user?.id,
-    notificationId
+    notificationId,
+    updateFn
   });
 
   return (
     <button
-        className="grid grid-cols-6 w-full p-2 border-2 gap-2 hover:bg-violet-200 text-start text-sm h-16 items-center"
+        className={clsx("rounded-lg mt-1 mb-1 grid grid-cols-6 w-full p-2 border-2 gap-2 hover:bg-blue-100 text-start text-sm h-16 items-center", {"font-semibold": !isRead})}
         key={notificationId}
         onClick={handleClick}
         >
