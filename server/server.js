5bf78e1960ae4ff103d2973c683f55796f196ff6
TIM GEENEN <timmiej_95@hotmail.com>
2024-09-16 16:40:43 +0700

    feat: added creation date to notification schema

--- server/server.js
+++ server/server.js
@@ -88,6 +88,10 @@ const userSchema = new mongoose.Schema({
         type: Boolean,
         default: false,
       },
+      createdAt: {
+        type: Date,
+        default: new Date()
+      }
     },
   ],
 });
