4f36ed3af7740199ebac548b2b247ba5d23eacab
TIM GEENEN <timmiej_95@hotmail.com>
2024-09-16 17:16:44 +0700

    fix: fixed problem where notification wouldnt get updated on clicking link

--- src/hooks/useNavigateNotification.jsx
+++ src/hooks/useNavigateNotification.jsx
@@ -3,14 +3,14 @@ import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 
 function useNavigateNotification({ teamId, taskId, userId, notificationId, updateFn }) {
-  const { socket } = useSelector(state => state.auth);
+  const { socket, user } = useSelector(state => state.auth);
   const navigate = useNavigate();
 
   const id = teamId ? teamId : taskId ? taskId : userId;
   const route = teamId ? `/team-info/${id}` : taskId ? `/task-info/${id}` : `/profile/${id}`
 
   const handleClick = useCallback(() => {
-    socket.emit("readNotification", userId, notificationId, (response) => {
+    socket.emit("readNotification", user._id, notificationId, (response) => {
       if (response.error) { return console.error(response.error.message) };
       if (updateFn) { updateFn() };
       navigate(route);
