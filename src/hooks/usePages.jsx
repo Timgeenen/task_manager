f12a9a628fac4a4fa37480a5a5f53b02a798ea57
TIM GEENEN <timmiej_95@hotmail.com>
2024-09-17 13:52:54 +0700

    feat: added lastPage variable to export

--- src/hooks/usePages.jsx
+++ src/hooks/usePages.jsx
@@ -1,11 +1,11 @@
-import { useEffect, useState } from "react"
+import { useState } from "react"
 
 function usePages(arrayLength, itemsPerPage) {
   const [lowestIndex, setLowestIndex] = useState(0)
   const [highestIndex, setHighestIndex] = useState(itemsPerPage - 1);
   const [page, setPage] = useState(1);
   const lastPage = Math.ceil((arrayLength) / itemsPerPage);
-console.log(lastPage, arrayLength)
+
   const nextPage = () => {
     if (page >= lastPage) { return }
     setPage(page + 1);
@@ -13,20 +13,13 @@ console.log(lastPage, arrayLength)
     setHighestIndex(highestIndex + itemsPerPage);
   };
 
-  useEffect(() => {
-    console.log(`page ${page}`)
-    console.log(`highest ${highestIndex}`)
-
-  }, [page, highestIndex])
-
   const prevPage = () => {
-    console.log("clicked")
     if (page <= 1) { return }
     setPage(page - 1);
     setLowestIndex(lowestIndex - itemsPerPage);
     setHighestIndex(highestIndex - itemsPerPage);
   };
-  return [prevPage, nextPage, lowestIndex, highestIndex, page];
+  return [prevPage, nextPage, lowestIndex, highestIndex, page, lastPage];
 }
 
 export default usePages
