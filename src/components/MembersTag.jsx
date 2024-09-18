66d718f35869d3592b199a777d69a9163034adb5
TIM GEENEN <timmiej_95@hotmail.com>
2024-09-18 11:27:10 +0700

    refactor: added specific styling for team manager

--- src/components/MembersTag.jsx
+++ src/components/MembersTag.jsx
@@ -1,8 +1,10 @@
 import { useNavigate } from "react-router-dom";
 import { COLORS } from "../library/constants";
 import { getInitials } from "../library/helperfunctions";
+import { MdAdminPanelSettings } from "react-icons/md";
 
-function MembersTag({ member, index, memberId }) {
+
+function MembersTag({ member, index, memberId, isManager }) {
   const initials = getInitials(member);
   const navigate = useNavigate();
   const navigateToProfile = () => {
@@ -11,11 +13,15 @@ function MembersTag({ member, index, memberId }) {
   return (
       <button
       key={memberId}
-      style={{background: COLORS[index]}}
+      style={{
+        background: isManager ? "gold" : COLORS[index],
+        color: isManager && "black",
+      }}
       onClick={navigateToProfile}
-      className="rounded-full p-1 w-9 h-9 -mr-4 text-sm font-semibold border-2 border-black text-white"
+      className="relative rounded-full p-1 w-9 h-9 -mr-4 text-sm font-semibold border-2 border-black text-white"
       >
         {initials}
+        {isManager && <MdAdminPanelSettings size={16} className="absolute -bottom-1 -right-1"/>}
       </button>
   )
 }
