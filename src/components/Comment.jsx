a6a1ba819b12e5758e9242b15607c3f82a365e6c
TIM GEENEN <timmiej_95@hotmail.com>
2024-09-18 15:55:18 +0700

    refactor: added clsx for combining styles

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -2,6 +2,7 @@ import { useSelector } from "react-redux"
 import { getTimePassed } from "../library/helperfunctions";
 import { useNavigate } from "react-router-dom";
 import { memo } from "react";
+import clsx from "clsx";
 
 function Comment({
   authorName,
@@ -15,17 +16,16 @@ function Comment({
   const classes = user._id === authorId
   ? "self-start bg-green-100 ml-2 mr-80"
   : "self-end bg-orange-100 mr-2 ml-80"
-  const standardClasses = "flex flex-col items-start p-2 rounded-lg border-2"
-  const allClasses = standardClasses + " " + classes
+
   const timePassed = getTimePassed(date);
 
   const navigateToUser = () => {
-    console.log(`navigated to ${authorName}'s user page`);
+    navigate(`/profile/${authorId}`)
   }
 
   return (
     <div 
-    className={allClasses}
+    className={clsx("flex flex-col items-start p-2 rounded-lg border-2", classes)}
     key={commentId}
     >
       <button
