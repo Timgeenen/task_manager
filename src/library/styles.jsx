cd8a8f5810014448f261b84cabab360be65b4fb2
TIM GEENEN <timmiej_95@hotmail.com>
2024-09-21 15:25:27 +0700

    style: added global grid col style

--- src/library/styles.jsx
+++ src/library/styles.jsx
@@ -1,3 +1,5 @@
 export const ellipsis = "overflow-ellipsis overflow-hidden whitespace-nowrap";
 
-export const errorMessage = "text-xs text-red-600"
+export const errorMessage = "text-xs text-red-600";
+
+export const gridCols = "grid grid-flow-col auto-cols-fr"
