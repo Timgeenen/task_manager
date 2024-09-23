4d2bd7e9028be9248afc507c8f6b5cb9698ca575
TIM GEENEN <timmiej_95@hotmail.com>
2024-09-23 20:36:52 +0700

    refactor: updated query and component to returned data from api acoordingly

--- src/pages/TeamInfo.jsx
+++ src/pages/TeamInfo.jsx
@@ -1,34 +1,35 @@
 import { useQuery } from "@tanstack/react-query";
 import { useNavigate, useParams } from "react-router-dom"
-import { getTeamsByIds } from "../api/Event";
+import { getTeamById } from "../api/Event";
 import MembersTag from "../components/MembersTag";
 import Chatroom from "../components/Chatroom";
 
 function TeamInfo() {
   const { teamId } = useParams();
   const navigate = useNavigate();
-  const { isSucces, isLoading, isError, error, data } = useQuery({
+
+  const { isLoading, isError, error, data } = useQuery({
     queryKey: [`team-${teamId}`],
-    queryFn: () => getTeamsByIds([teamId])
+    queryFn: () => getTeamById(teamId)
   });
 
   if(isError) {console.error(error.message)};
-  data && console.log(data[0]);
+
   return (
     <div className="w-full h-full flex justify-center">
       {isLoading && <div>Loading...</div>}
       {data && 
         <div className="w-3/4 mt-8">
           <div className="mb-4">
-            <h1 className="text-3xl font-semibold m-2 mb-6">{data[0]?.name}</h1>
+            <h1 className="text-3xl font-semibold m-2 mb-6">{data?.name}</h1>
             <div className="flex w-full shadow-lg rounded-lg p-2">
               <div className="w-60 flex flex-col gap-y-1 p-2 overflow-y-scroll">
                 <User
-                member={data[0].manager}
+                member={data?.manager}
                 isManager={true}
                 />
-                {data[0]?.members.map((member, i) =>
-                member.id !== data[0].manager.id && (
+                {data?.members.map((member, i) =>
+                member.id !== data.manager.id && (
                   <User
                   member={member}
                   isManager={false}
@@ -46,7 +47,7 @@ function TeamInfo() {
                   <span>Deadline</span>
                 </span>
                 <div className="flex flex-col h-52 overflow-y-scroll gap-1 mt-1">
-                {data[0]?.tasks.map((task, i) => (
+                {data?.tasks.map((task, i) => (
                   <button
                   className="grid grid-flow-col auto-cols-fr mr-1 ml-1 rounded-full shadow-md p-1 hover:bg-blue-300"
                   onClick={() => navigate(`/task-info/${task.id}`)}
