034c50f1500d0a77e4fdd7f9cb2aff03256db60b
TIM GEENEN <timmiej_95@hotmail.com>
2024-09-29 17:54:12 +0700

    refactor: extracted io socket routes into seperate file

---
+++ server/socket.js
@@ -0,0 +1,328 @@
+const func = require("./functions");
+const jwt = require("jsonwebtoken");
+const { User, Team, Task } = require("./mongoose");
+const { Server } = require("socket.io");
+
+const initializeSocket = (httpServer) => {
+
+  const io = new Server(httpServer, {
+    cors: {
+      origin: "*",
+      methods: ["GET", "POST", "PUT", "DELETE"],
+    },
+  });
+
+  io.on("connection", (socket) => {
+    console.log("connecting")
+    const { accessToken } = func.getCookies(socket.handshake.headers.cookie);
+    const { myId } = jwt.decode(accessToken);
+    const type = socket.handshake.auth.type;
+
+    if (type === "login") {
+      User.findByIdAndUpdate(myId, { $set: { isActive: true } })
+        .then((user) => {
+          socket.join(user._id);
+          console.log(`${user.name} joined notification channel`);
+          user.teams.map((team) => {
+            socket.join(team.id);
+            console.log(`${user.name} joined ${team.name}`);
+          });
+        })
+        .catch((err) => console.error(err.message));
+    }
+
+    socket.on("createTeam", async (teamData, callback) => {
+      const { name, manager, members } = teamData;
+
+      try {
+        const team = await Team.create({
+          name,
+          manager,
+          members,
+          tasks: [],
+          createdOn: func.newDate(),
+          messages: [],
+          trash: [],
+        });
+
+        const teamObj = {
+          name,
+          members,
+          id: team._id,
+          managerId: manager.id,
+        };
+
+        const ids = members.map((member) => member.id);
+
+        const notificationObj = {
+          nType: "New Team",
+          team: {
+            name: team.name,
+            id: team._id,
+          },
+          message: `You have been added to ${team.name}, created by ${team.manager.name}`,
+        };
+
+        await User.updateMany(
+          { _id: { $in: ids } },
+          {
+            $push: {
+              teams: teamObj,
+              notifications: { $each: [notificationObj], $position: 0 },
+            },
+          }
+        );
+
+        const user = await User.findById(manager.id);
+
+        ids.map((id) => io.to(id).emit("receiveNotification", notificationObj));
+
+        callback({ user });
+      } catch (err) {
+        callback({
+          error: err,
+        });
+      }
+    });
+
+    socket.on("createTask", async (taskData, callback) => {
+      const {
+        subtasks,
+        title,
+        team,
+        description,
+        deadline,
+        members,
+        priority,
+        user,
+      } = taskData;
+
+      const ids = members.map((member) => member.id);
+
+      const data = {
+        title,
+        description,
+        subtasks,
+        createdOn: func.newDate(),
+        deadline,
+        priority,
+        status: "pending",
+        assignedTo: members,
+        assignedTeam: team,
+        comments: [],
+        updates: [],
+      };
+
+      try {
+        const newTask = await Task.create(data);
+
+        const notificationObj = {
+          nType: "New Task",
+          task: {
+            name: title,
+            id: newTask._id,
+          },
+          message: `${user} has added a new task to ${team.name}`,
+        };
+
+        const taskPointer = {
+          title,
+          priority,
+          status: "pending",
+          deadline,
+          id: newTask._id,
+        };
+
+        await Team.findByIdAndUpdate(team.id, {
+          $push: { tasks: taskPointer },
+        });
+
+        await User.updateMany(
+          { _id: { $in: ids } },
+          {
+            $push: {
+              notifications: { $each: [notificationObj], $position: 0 },
+            },
+          }
+        );
+
+        io.to(team.id).emit("receiveNotification", notificationObj);
+        callback({
+          message: "succesfully created task",
+        });
+      } catch (err) {
+        callback({
+          error: err,
+        });
+      }
+    });
+
+    socket.on("updateTask", async (taskData, callback) => {
+      const { taskId, teamId, subtasks, description, completed } = taskData;
+
+      try {
+        const task = await Task.findById(taskId);
+        const user = await User.findById(myId, {
+          name: 1,
+        });
+
+        const updateObj = {
+          author: {
+            name: user.name,
+            id: user._id,
+          },
+          previousState: {
+            description: task.description,
+            subtasks: task.subtasks,
+            status: task.status,
+          },
+          updatedAt: func.newDate(),
+        };
+
+        task.updates.unshift(updateObj);
+        task.subtasks = subtasks;
+        task.description = description;
+
+        if (completed) {
+          task.status = "completed";
+        }
+
+        await task.save();
+
+        const notificationObj = {
+          nType: completed ? "Task Completed" : "Task Updated",
+          task: {
+            name: task.title,
+            id: task._id,
+          },
+          message: completed
+            ? `${task.title} has been completed`
+            : `${user.name} has updated ${task.title}`,
+        };
+
+        if (completed) {
+          await Team.updateOne(
+            { _id: teamId, "tasks.id": taskId },
+            {
+              $set: { "tasks.$.status": "completed" },
+            }
+          );
+        }
+
+        const ids = task.assignedTo.map((member) => member.id);
+
+        await User.updateMany(
+          { _id: { $in: ids } },
+          {
+            $push: {
+              notifications: { $each: [notificationObj], $position: 0 },
+            },
+          }
+        );
+
+        io.to(task.assignedTeam.id).emit(
+          "receiveNotification",
+          notificationObj
+        );
+
+        callback(updateObj);
+      } catch (error) {
+        callback(error);
+      }
+    });
+
+    socket.on("readNotification", async (notificationId, callback) => {
+      try {
+        await User.updateOne(
+          { _id: myId, "notifications._id": notificationId },
+          { $set: { "notifications.$.isRead": true } }
+        );
+        callback({});
+      } catch (error) {
+        callback({ error });
+      }
+    });
+
+    socket.on("markAllAsRead", async (callback) => {
+      try {
+        const res = await User.updateOne(
+          { _id: myId },
+          {
+            $set: { "notifications.$[notification].isRead": true },
+          },
+          {
+            arrayFilters: [{ "notification.isRead": false }],
+          }
+        );
+
+        callback({ res });
+      } catch (err) {
+        callback(err);
+      }
+    });
+
+    socket.on("deleteReadNotifications", async (callback) => {
+      try {
+        await User.findByIdAndUpdate(myId, {
+          $pull: { notifications: { isRead: true } },
+        });
+        callback("success");
+      } catch (err) {
+        callback(err);
+      }
+    });
+
+    socket.on("joinNewTeam", (teamId) => {
+      socket.join(teamId);
+      console.log(`You joined a new team: ${teamId}`);
+    });
+
+    socket.on("joinTaskRoom", (taskId) => {
+      socket.join(taskId);
+      console.log(`You joined the task chat for task: ${taskId}`);
+    });
+
+    socket.on("sendMessage", async (messageObj) => {
+      const { author, message, socketId, socketType } = messageObj;
+      const comment = {
+        author,
+        message,
+        createdAt: func.newDate(),
+      };
+
+      try {
+        let updatedDocument;
+        const query = {
+          $push: { comments: { $each: [comment], $position: 0 } },
+        };
+
+        if (socketType === "task") {
+          updatedDocument = await Task.findByIdAndUpdate(socketId, query, {
+            new: true,
+          });
+        }
+
+        if (socketType === "team") {
+          updatedDocument = await Team.findByIdAndUpdate(socketId, query, {
+            new: true,
+          });
+        }
+
+        io.to(socketId).emit("receiveMessage", updatedDocument.comments[0]);
+      } catch (err) {
+        console.error(err.message);
+      }
+    });
+
+    socket.on("disconnect", async () => {
+      await User.findByIdAndUpdate(myId, {
+        $set: { isActive: false, updatedAt: new Date() },
+      });
+      console.log(`You have been disconnected`);
+    });
+  });
+
+  return io
+};
+
+module.exports = initializeSocket;
