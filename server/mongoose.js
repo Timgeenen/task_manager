28151ce25716182875ea97a21c10c6055295f976
TIM GEENEN <timmiej_95@hotmail.com>
2024-10-10 15:06:51 +0700

    refactor: updated user and team documents

--- server/mongoose.js
+++ server/mongoose.js
@@ -193,7 +193,7 @@ const userSchema = new mongoose.Schema(
           required: true,
           trim: true,
           minLength: [3, "message must be at least 3 characters long"],
-          maxLength: [254, "message can be a maximum of 254 characters long"]
+          maxLength: [254, "message can be a maximum of 254 characters long"],
         },
         isRead: {
           type: Boolean,
@@ -213,41 +213,17 @@ const userSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-const teamSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    // match: ["/^[A-Za-z0-9 ]*$/", "name cannot contain any special characters"],
-    minLength: [8, "team name must be at least 8 characters long"],
-    maxLength: [50, "team name can be a maximum of 50 characters long"],
-    trim: true,
-  },
-  manager: {
+const teamSchema = new mongoose.Schema(
+  {
     name: {
       type: String,
       required: true,
+      // match: ["/^[A-Za-z0-9 ]*$/", "name cannot contain any special characters"],
+      minLength: [8, "team name must be at least 8 characters long"],
+      maxLength: [50, "team name can be a maximum of 50 characters long"],
       trim: true,
     },
-    role: {
-      type: String,
-      required: true,
-      trim: true,
-    },
-    email: {
-      type: String,
-      required: true,
-      trim: true,
-    },
-    id: {
-      type: String,
-      required: true,
-      trim: true,
-      ref: "User",
-    },
-    _id: false,
-  },
-  members: [
-    {
+    manager: {
       name: {
         type: String,
         required: true,
@@ -267,130 +243,184 @@ const teamSchema = new mongoose.Schema({
         type: String,
         required: true,
         trim: true,
-        ref: "User"
+        ref: "User",
       },
       _id: false,
     },
-  ],
-  tasks: [
-    {
-      title: {
-        type: String,
-        required: true,
-        trim: true
-      },
-      priority: {
-        type: String,
-        required: true,
-        trim: true,
-        enum: ["low", "medium", "high"]
-      },
-      status: {
-        type: String,
-        required: true,
-        trim: true,
-        enum: ["pending", "in progress", "completed"]
-      },
-      deadline: {
-        type: Date,
-        required: true,
-        trim: true
-      },
-      id: {
-        type: String,
-        required: true,
-        trim: true,
-        ref: "Task"
-      },
-      _id: false,
-    },
-  ],
-  comments: [
-    {
-      author: {
+    members: [
+      {
         name: {
           type: String,
           required: true,
-          trim: true
+          trim: true,
+        },
+        role: {
+          type: String,
+          required: true,
+          trim: true,
+        },
+        email: {
+          type: String,
+          required: true,
+          trim: true,
         },
         id: {
           type: String,
           required: true,
           trim: true,
-          ref: "User"
+          ref: "User",
         },
         _id: false,
       },
-      message: {
-        type: String,
-        required: true,
-        minLength: [1, "message must contain at least 1 char"],
-        maxLength: [500, "message can contain a maximum of 500 characters"]
+    ],
+    tasks: [
+      {
+        title: {
+          type: String,
+          required: true,
+          trim: true,
+        },
+        priority: {
+          type: String,
+          required: true,
+          trim: true,
+          enum: ["low", "medium", "high"],
+        },
+        status: {
+          type: String,
+          required: true,
+          trim: true,
+          enum: ["pending", "in progress", "completed"],
+        },
+        deadline: {
+          type: Date,
+          required: true,
+          trim: true,
+        },
+        id: {
+          type: String,
+          required: true,
+          trim: true,
+          ref: "Task",
+        },
+        _id: false,
       },
-      createdAt: {
-        type: Date,
-        default: new Date(),
+    ],
+    comments: [
+      {
+        author: {
+          name: {
+            type: String,
+            required: true,
+            trim: true,
+          },
+          id: {
+            type: String,
+            required: true,
+            trim: true,
+            ref: "User",
+          },
+          _id: false,
+        },
+        message: {
+          type: String,
+          required: true,
+          minLength: [1, "message must contain at least 1 char"],
+          maxLength: [500, "message can contain a maximum of 500 characters"],
+        },
+        createdAt: {
+          type: Date,
+          default: new Date(),
+        },
       },
-    },
-  ],
-  trash: [Map],
-}, { timestamps: true});
+    ],
+  },
+  { timestamps: true }
+);
 
-const taskSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
-    trim: true,
-    // match: [
-    //   "/^[A-Za-z0-9 ]*$/",
-    //   "task name cannot contain any special characters",
-    // ],
-    minLength: [8, "task name must be at least 8 characters long"],
-    maxLength: [50, "task name can be a maximum of 50 characters long"],
-  },
-  description: {
-    type: String,
-    required: true,
-    minLength: [1, "task description must contain at least 1 character"],
-  },
-  subtasks: [
-    {
-      name: {
-        type: String,
-        required: true,
-        trim: true,
-      },
-      completed: {
-        type: Boolean,
-        default: false,
-      },
+const taskSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+      // match: [
+      //   "/^[A-Za-z0-9 ]*$/",
+      //   "task name cannot contain any special characters",
+      // ],
+      minLength: [8, "task name must be at least 8 characters long"],
+      maxLength: [50, "task name can be a maximum of 50 characters long"],
     },
-  ],
-  deadline: {
-    type: Date,
-    required: true,
-    trim: true,
-  },
-  priority: {
-    type: String,
-    required: true,
-    trim: true,
-    enum: ["low", "medium", "high"],
-  },
-  status: {
-    type: String,
-    required: true,
-    trim: true,
-    enum: ["pending", "in progress", "completed"],
-  },
-  assignedTo: [
-    {
+    description: {
+      type: String,
+      required: true,
+      minLength: [1, "task description must contain at least 1 character"],
+    },
+    subtasks: [
+      {
+        name: {
+          type: String,
+          required: true,
+          trim: true,
+        },
+        completed: {
+          type: Boolean,
+          default: false,
+        },
+      },
+    ],
+    deadline: {
+      type: Date,
+      required: true,
+      trim: true,
+    },
+    priority: {
+      type: String,
+      required: true,
+      trim: true,
+      enum: ["low", "medium", "high"],
+    },
+    status: {
+      type: String,
+      required: true,
+      trim: true,
+      enum: ["pending", "in progress", "completed"],
+    },
+    assignedTo: [
+      {
+        name: {
+          type: String,
+          required: true,
+          trim: true,
+        },
+        id: {
+          type: String,
+          required: true,
+          trim: true,
+          ref: "User",
+        },
+        _id: false,
+      },
+    ],
+    workingOnTask: [
+      {
+        type: String,
+        ref: "User",
+      },
+    ],
+    assignedTeam: {
       name: {
         type: String,
         required: true,
         trim: true,
       },
       id: {
+        type: String,
+        required: true,
+        trim: true,
+        ref: "Team",
+      },
+      managerId: {
         type: String,
         required: true,
         trim: true,
@@ -398,108 +428,83 @@ const taskSchema = new mongoose.Schema({
       },
       _id: false,
     },
-  ],
-  workingOnTask: [
-    {
-      type: String,
-      ref: "User"
-    }
-  ],
-  assignedTeam: {
-    name: {
-      type: String,
-      required: true,
-      trim: true,
-    },
-    id: {
-      type: String,
-      required: true,
-      trim: true,
-      ref: "Team",
-    },
-    managerId: {
-      type: String,
-      required: true,
-      trim: true,
-      ref: "User",
-    },
-    _id: false,
-  },
-  comments: [
-    {
-      author: {
-        name: {
-          type: String,
-          required: true,
-          trim: true,
-        },
-        id: {
-          type: String,
-          required: true,
-          trim: true,
-          ref: "User",
-        },
-        _id: false,
-      },
-      message: {
-        type: String,
-        required: true,
-        trim: true,
-        minlength: [1, "message must be at least 1 char long"],
-      },
-      createdAt: {
-        type: Date,
-        required: true,
-        default: new Date(),
-        trim: true,
-      },
-    },
-  ],
-  updates: [
-    {
-      author: {
-        name: {
-          type: String,
-          required: true,
-          trim: true,
-        },
-        id: {
-          type: String,
-          required: true,
-          trim: true,
-          ref: "User",
-        },
-        _id: false,
-      },
-      previousState: {
-        description: {
-          type: String,
-          required: true,
-          trim: true
-        },
-        subTasks: [
-          {
-            name: {
-              type: String,
-              required: true,
-              trim: true
-            },
-            completed: {
-              type: Boolean,
-              required: true
-            },
+    comments: [
+      {
+        author: {
+          name: {
+            type: String,
+            required: true,
+            trim: true,
           },
-        ],
-        status: {
+          id: {
+            type: String,
+            required: true,
+            trim: true,
+            ref: "User",
+          },
+          _id: false,
+        },
+        message: {
           type: String,
           required: true,
           trim: true,
-          enum: ["pending", "in progress", "completed"]
+          minlength: [1, "message must be at least 1 char long"],
+        },
+        createdAt: {
+          type: Date,
+          required: true,
+          default: new Date(),
+          trim: true,
         },
       },
-    },
-  ],
-}, { timestamps: true });
+    ],
+    updates: [
+      {
+        author: {
+          name: {
+            type: String,
+            required: true,
+            trim: true,
+          },
+          id: {
+            type: String,
+            required: true,
+            trim: true,
+            ref: "User",
+          },
+          _id: false,
+        },
+        previousState: {
+          description: {
+            type: String,
+            required: true,
+            trim: true,
+          },
+          subTasks: [
+            {
+              name: {
+                type: String,
+                required: true,
+                trim: true,
+              },
+              completed: {
+                type: Boolean,
+                required: true,
+              },
+            },
+          ],
+          status: {
+            type: String,
+            required: true,
+            trim: true,
+            enum: ["pending", "in progress", "completed"],
+          },
+        },
+      },
+    ],
+  },
+  { timestamps: true }
+);
 
 let User = mongoose.model("user", userSchema);
 let Team = mongoose.model("team", teamSchema);
