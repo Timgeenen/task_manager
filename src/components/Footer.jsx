3eee49e7cd123899ef21a6aafae284c3639e18bd
TIM GEENEN <timmiej_95@hotmail.com>
2024-10-14 18:18:57 +0700

    feat: added icon and moved images to public folder

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,8 +11,8 @@ function Footer() {
         >Tim Geenen</a>
       </div>
       <div className="flex gap-2 sm:gap-4">
-        <a className="w-4 sm:w-6"><img src="src/assets/linkedin_logo.png"/></a>
-        <a className="w-4 sm:w-6"><img src="src/assets/github_logo.png" className="bg-white rounded-full"/></a>
+        <a className="w-4 sm:w-6"><img src="../../public/linkedin_logo.png"/></a>
+        <a className="w-4 sm:w-6"><img src="../../public/github_logo.png" className="bg-white rounded-full"/></a>
       </div>
     </div>
   )
